f11d551c3fa225f18369ba7c9662765ce82e0451
majlv <91908439+majlv@users.noreply.github.com>
2021-10-04 12:47:26 -0500

    add acctsRcvbl to structure

--- importStructure.js
+++ importStructure.js
@@ -1,12 +1,15 @@
 function importStructure(context) {
     var builder = context.getStructureBuilder();
+    var table = '';
+    var i = 0;
  
-    var table = builder.addTable('acctsPybl');
+    // Build Accounts Payable Table
+    table = builder.addTable('acctsPybl');
     table.setDisplayName('AccountsPayable');    
 
     ai.log.logInfo(`Creating table ${table.id}`);
     
-    var i=0;
+    i=0;
     createColumn(table  , "id"              , "ID"          , i++   , true , "string");
     createColumn(table  , "tx_date"         , "tx_date"     , i++   , true , "date");
     createColumn(table  , "amount"          , "amount"      , i++   , true , "number");
@@ -17,6 +20,28 @@ function importStructure(context) {
     createColumn(table  , "memo"            , "memo"        , i++   , true , "string");
     createColumn(table  , "split_acc"       , "split_acc"   , i++   , true , "string");
     
+    ai.log.logInfo(`${table.id} table created successfully`);
+    
+    // Build Accounts Payable Table
+    table = builder.addTable('acctsRcvbl');
+    table.setDisplayName('AccountsReceivable');    
+
+    ai.log.logInfo(`Creating table ${table.id}`);
+    
+    i=0;
+    createColumn(table  , "id"              , "ID"          , i++   , true , "string");
+    createColumn(table  , "tx_date"         , "tx_date"     , i++   , true , "date");
+    createColumn(table  , "amount"          , "amount"      , i++   , true , "number");
+    createColumn(table  , "balance"         , "balance"     , i++   , true , "number");
+    createColumn(table  , "tx_type"         , "tx_type"     , i++   , true , "string");
+    createColumn(table  , "doc_num"         , "doc_num"     , i++   , true , "string");
+    createColumn(table  , "name"            , "name"        , i++   , true , "string");
+    createColumn(table  , "memo"            , "memo"        , i++   , true , "string");
+    createColumn(table  , "split_acc"       , "split_acc"   , i++   , true , "string");
+    
+    ai.log.logInfo(`${table.id} table created successfully`);
+    
+    // createColumn Function
     function createColumn(table, columnId, columnDisplay, displayOrder, reqImport, dataType) {
         var column = table.addColumn(columnId);
         column.setDisplayName(columnDisplay);
@@ -31,5 +56,4 @@ function importStructure(context) {
             default : ai.log.logInfo('Invalid Column', 'Column ' + columnID); break;
         }
     }
-    ai.log.logInfo(`${table.id} table created successfully`);
 }
