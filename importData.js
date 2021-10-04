5d04d6271289c819413f6e964132df8bfa8242ed
joselvelez <joselvelez@protonmail.com>
2021-10-04 12:40:05 -0500

    revise importData script

--- importData.js
+++ importData.js
@@ -4,59 +4,74 @@ function importData(context) {
     var apiEndpoint = dataSource.getSetting("apiEndpoint").getValue();
     var startDateString = dataSource.getSetting("Period Range").getValue().getFromPeriodStartDateTime().substring(0, 10);
     var endDateString = dataSource.getSetting("Period Range").getValue().getToPeriodEndDateTime().substring(0, 10);
-    // var requestURL = `${apiEndpoint}/v3/company/${realmID}/reports/GeneralLedger?start_date=${startDateString}&end_date=${endDateString}&accounting_method=Accrual&account=33&minorversion=62`;
     var method = 'GET';
     var body = '';
     var headers = { };
     
     var tableId = context.getRowset(['id']).getTableId();
-    ai.log.logInfo('Table', 'Table Name ' + tableId);
 
-    var columnsMap = [];
-    var dataCell = [];
+    // var columnsMap = [];
+    // var dataCell = [];
     
-    if (tableId == 'acctsPybl') {
-        var actRowset = context.getRowset(["tx_date", "amount", "balance", "tx_type", "doc_num", "name", "memo", "split_acc"]);
-        var apAmtColumns = actRowset.getColumns();
-        
-        var sourceAccountId = 33;
-        requestURL = `${apiEndpoint}/v3/company/${realmID}/reports/GeneralLedger?start_date=${startDateString}&end_date=${endDateString}&accounting_method=Accrual&account=${sourceAccountId}&minorversion=62`;
-        var response = ai.https.authorizedRequest(requestURL, method, body, headers);
-        
-        try {
-            ai.log.logInfo("Trying to get data from source...", `Connecting to ${apiEndpoint}`);
-            response;
-        }
-        catch (exception) {
-            ai.log.logError('HTTPS connection request failed', ''+exception);
-            throw "Connection Error";
-        }
-        
-        if (response.getHttpCode() == '200') {
-            ai.log.logVerbose('Connection successful. Retrieving data...');
-            var responseBody = response.getBody();
-            // Locate the embedded object within the JSON response containing the rows of data
-            var importData = JSON.parse(responseBody).Rows.Row[0].Rows.Row;
+    // Accounts Payable
+    var apTable = 'acctsPybl';
+    var apAccountId = 33;
+    var apRowset = ["tx_date", "amount", "balance", "tx_type", "doc_num", "name", "memo", "split_acc"];
+    importData(apTable, apAccountId, apRowset);
+    
+    // Accounts Receivable
+    var arTable = 'acctsRcvbl';
+    var arAccountId = 84;
+    var arRowset = ["tx_date", "amount", "balance", "tx_type", "doc_num", "name", "memo", "split_acc"];
+    importData(arTable, arAccountId, arRowset);
+    
+    // Import Data Function
+    function importData(table, acctId, colArray) {
+        if (tableId == table) {
+            var dataRowset = context.getRowset(colArray);
+            var dataColumns = dataRowset.getColumns();
             
-            ai.log.logInfo('Getting row count...', `${importData.length} rows`);
+            // Get indices for each column for a column map
             
-            for (ap = 0; ap < importData.length; ap++) {
-                actRowset.addRow(
-                    [
-                        new Date(importData[ap].ColData[0].value),
-                        Number(importData[ap].ColData[6].value),
-                        Number(importData[ap].ColData[7].value),
-                        importData[ap].ColData[1].value,
-                        importData[ap].ColData[2].value,
-                        importData[ap].ColData[3].value,
-                        importData[ap].ColData[4].value,
-                        importData[ap].ColData[5].value
-                    ]
-                );
+            
+            requestURL = `${apiEndpoint}/v3/company/${realmID}/reports/GeneralLedger?start_date=${startDateString}&end_date=${endDateString}&accounting_method=Accrual&account=${acctId}&minorversion=62`;
+            var response = ai.https.authorizedRequest(requestURL, method, body, headers);
+            
+            try {
+                ai.log.logInfo("Trying to get data from source...", `Connecting to ${apiEndpoint}`);
+                response;
             }
-        } else {
-            ai.log.logError('Error retrieving data from source.', +exception);
-            throw "Error";
+            catch (exception) {
+                ai.log.logError('HTTPS connection request failed', ''+exception);
+                throw "Connection Error";
+            }
+            
+            if (response.getHttpCode() == '200') {
+                ai.log.logVerbose('Connection successful. Retrieving data...');
+                var responseBody = response.getBody();
+                // Locate the embedded object within the JSON response containing the rows of data
+                var data = JSON.parse(responseBody).Rows.Row[0].Rows.Row;
+                
+                ai.log.logInfo('Getting row count...', `${data.length} rows`);
+                
+                for (i = 0; i < data.length; i++) {
+                    dataRowset.addRow(
+                        [
+                            new Date(data[i].ColData[0].value),
+                            Number(data[i].ColData[6].value),
+                            Number(data[i].ColData[7].value),
+                            data[i].ColData[1].value,
+                            data[i].ColData[2].value,
+                            data[i].ColData[3].value,
+                            data[i].ColData[4].value,
+                            data[i].ColData[5].value
+                        ]
+                    );
+                }
+            } else {
+                ai.log.logError('Error retrieving data from source.', +exception);
+                throw "Error";
+            }
         }
     }
 }
