b968114d919bfff7010605b15805c7319b53a945
joselvelez <joselvelez@protonmail.com>
2021-10-04 12:26:31 -0500

    revise importStructure

--- importStructure.js
+++ importStructure.js
@@ -2,67 +2,33 @@ function importStructure(context) {
     var builder = context.getStructureBuilder();
  
     ai.log.logInfo('Creating table...');
-    var table = builder.addTable('Accounts Payable (A/P)');
-    table.setDisplayName('Accounts Payable');    
+    var table = builder.addTable('acctsPybl');
+    table.setDisplayName('AccountsPayable');    
  
-    ai.log.logInfo('Adding columns...');
-    var recordColumn = null;
+    var i=0;
+    createColumn(table  , "id"              , "ID"          , i++   , true , "string");
+    createColumn(table  , "tx_date"         , "tx_date"     , i++   , true , "date");
+    createColumn(table  , "amount"          , "amount"      , i++   , true , "number");
+    createColumn(table  , "balance"         , "balance"     , i++   , true , "number");
+    createColumn(table  , "tx_type"         , "tx_type"     , i++   , true , "string");
+    createColumn(table  , "doc_num"         , "doc_num"     , i++   , true , "string");
+    createColumn(table  , "name"            , "name"        , i++   , true , "string");
+    createColumn(table  , "memo"            , "memo"        , i++   , true , "string");
+    createColumn(table  , "split_acc"       , "split_acc"   , i++   , true , "string");
     
-    recordColumn = table.addColumn('tx_date');
-    recordColumn.setDisplayName('tx_date');
-    recordColumn.setDisplayOrder('0');
-    recordColumn.setIncludeByDefault(true); 
-    recordColumn.setRemoteColumnType('dateTime');
-    recordColumn.setDateTimeColumnType();
-    
-    recordColumn = table.addColumn('amount');
-    recordColumn.setDisplayName('amount');
-    recordColumn.setDisplayOrder('1');
-    recordColumn.setIncludeByDefault(true); 
-    recordColumn.setRemoteColumnType('float');
-    recordColumn.setFloatColumnType();
-    
-    recordColumn = table.addColumn('balance');
-    recordColumn.setDisplayName('balance');
-    recordColumn.setDisplayOrder('2');
-    recordColumn.setIncludeByDefault(true); 
-    recordColumn.setRemoteColumnType('float');
-    recordColumn.setFloatColumnType();
-    
-    recordColumn = table.addColumn('tx_type');
-    recordColumn.setDisplayName('tx_type');
-    recordColumn.setDisplayOrder('3');
-    recordColumn.setIncludeByDefault(true); 
-    recordColumn.setRemoteColumnType('text');
-    recordColumn.setTextColumnType(60);
-    
-    recordColumn = table.addColumn('doc_num');
-    recordColumn.setDisplayName('doc_num');
-    recordColumn.setDisplayOrder('4');
-    recordColumn.setIncludeByDefault(true); 
-    recordColumn.setRemoteColumnType('text');
-    recordColumn.setTextColumnType(60);
-    
-    recordColumn = table.addColumn('name');
-    recordColumn.setDisplayName('name');
-    recordColumn.setDisplayOrder('5');
-    recordColumn.setIncludeByDefault(true); 
-    recordColumn.setRemoteColumnType('text');
-    recordColumn.setTextColumnType(60);
-    
-    recordColumn = table.addColumn('memo');
-    recordColumn.setDisplayName('memo');
-    recordColumn.setDisplayOrder('6');
-    recordColumn.setIncludeByDefault(true); 
-    recordColumn.setRemoteColumnType('text');
-    recordColumn.setTextColumnType(120);
-    
-    recordColumn = table.addColumn('split_acc');
-    recordColumn.setDisplayName('split_acc');
-    recordColumn.setDisplayOrder('7');
-    recordColumn.setIncludeByDefault(true); 
-    recordColumn.setRemoteColumnType('text');
-    recordColumn.setTextColumnType(60);
-    
+    function createColumn(table, columnId, columnDisplay, displayOrder, reqImport, dataType) {
+        var column = table.addColumn(columnId);
+        column.setDisplayName(columnDisplay);
+        column.setDisplayOrder(displayOrder);
+        column.setMandatoryForImports(reqImport);
+        switch(dataType) {
+            case "integer"  : column.setIntegerColumnType();    break;
+            case "string"   : column.setTextColumnType();       break;
+            case "date"     : column.setDateTimeColumnType();   break;
+            case "boolean"  : column.setBooleanColumnType();    break;
+            case "number"   : column.setFloatColumnType();      break;
+            default : ai.log.logInfo('Invalid Column', 'Column ' + columnID); break;
+        }
+    }
     ai.log.logInfo('Table created successfully');
 }
